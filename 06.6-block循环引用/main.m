2fd2b077bd9c132a9f7717dd4f20a069de41ebc3
刘光强 <1126756952@qq.com>
2020-02-06 14:04:04 +0800

    Initial Commit

---
+++ 06.6-block循环引用/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  06.6-block循环引用
+//
+//  Created by 刘光强 on 2020/2/6.
+//  Copyright © 2020 guangqiang.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
